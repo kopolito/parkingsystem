9edb5fd7aae1f7e722c37c47f8ca3667cd0bdf7f
kopolito <dev@kopol.xyz>
2020-10-09 14:53:14 +0200

    coverage : added InteractiveShell incoming test

--- src/test/java/com/parkit/parkingsystem/unit/service/InteractiveShellTest.java
+++ src/test/java/com/parkit/parkingsystem/unit/service/InteractiveShellTest.java
@@ -1,6 +1,7 @@
 package com.parkit.parkingsystem.unit.service;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayOutputStream;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
@@ -56,6 +58,20 @@ class InteractiveShellTest {
 
 		assertTrue(out.toString()
 				.contains("Exiting from the system!"));
+	}
+
+	@Test
+	void loadInterface_callsParkingServiceprocessIncomingVehicle_whenInputOne() {
+		when(inputReaderUtil.readSelection())
+				.thenReturn(1)
+				.thenReturn(3);
+
+		final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		interactiveShell.loadInterface();
+
+		verify(parkingService, Mockito.times(1)).processIncomingVehicle();
 	}
 
 }
