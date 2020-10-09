7bfac2e4b3bab96eb168fdb6d76ba50672a79234
kopolito <dev@kopol.xyz>
2020-10-09 15:48:34 +0200

    coverage : moved fare and parking service tests into unit.service package

--- src/test/java/com/parkit/parkingsystem/unit/service/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/unit/service/FareCalculatorServiceTest.java
@@ -1,4 +1,4 @@
-package com.parkit.parkingsystem;
+package com.parkit.parkingsystem.unit.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
