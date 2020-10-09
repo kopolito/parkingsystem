d0465f8157609e9bc79e27c6cde23b0414e4cbc2
kopolito <dev@kopol.xyz>
2020-10-09 14:51:16 +0200

    coverage : added InteractiveShell quit test

---
+++ src/test/java/com/parkit/parkingsystem/unit/service/InteractiveShellTest.java
@@ -0,0 +1,61 @@
+package com.parkit.parkingsystem.unit.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
+import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.service.InteractiveShell;
+import com.parkit.parkingsystem.service.ParkingService;
+import com.parkit.parkingsystem.util.InputReaderUtil;
+
+@ExtendWith(MockitoExtension.class)
+class InteractiveShellTest {
+
+	private static InteractiveShell interactiveShell;
+	private static PrintStream outOriginal;
+
+	@Mock
+	private static InputReaderUtil inputReaderUtil;
+	@Mock
+	private static ParkingSpotDAO parkingSpotDAO;
+	@Mock
+	private static TicketDAO ticketDAO;
+	@Mock
+	private static ParkingService parkingService;
+
+	@BeforeEach
+	private void setUpPerTest() {
+		outOriginal = System.out;
+		interactiveShell = new InteractiveShell(inputReaderUtil, parkingService);
+	}
+
+	@AfterEach
+	private void tearDown() {
+		System.setOut(outOriginal);
+	}
+
+	@Test
+	void loadInterface_exits_whenInputThree() {
+		when(inputReaderUtil.readSelection()).thenReturn(3);
+
+		final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		interactiveShell.loadInterface();
+
+		assertTrue(out.toString()
+				.contains("Exiting from the system!"));
+	}
+
+}
